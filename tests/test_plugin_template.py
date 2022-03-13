1f800e419c9f7ea36655345d6e5d8a1a582df492
Just Call Me Koko <at.the.red.team@gmail.com>
2022-03-13 18:46:00 -0400

    Add more test to template

--- tests/test_plugin_template.py
+++ tests/test_plugin_template.py
@@ -6,10 +6,18 @@ sys.path.append(os.path.abspath('plugins'))
 from plugin_template import Template
 
 def test_check_bits():
-	print('Running test template')
-
 	template = Template()
-
 	assert template.checkBits(0) == False
+
+def test_check_cat_true():
+	template = Template()
 	assert template.checkCat('admin') == True
+
+def test_check_cat_false():
+	template = Template()
 	assert template.checkCat('arrow') == False
+
+def test_raise_exception_check_cat():
+	template = Template()
+	with pytest.raises(TypeError):
+		template.checkCat(0)
