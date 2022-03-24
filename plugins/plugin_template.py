209b5df862aaa7166ccc3e2d183d2e35a996dab0
Just Call Me Koko <at.the.red.team@gmail.com>
2022-03-24 10:50:10 -0400

    Move constructor to modular

--- plugins/plugin_template.py
+++ plugins/plugin_template.py
@@ -50,13 +50,13 @@ class Template():
 			if (os.path.isfile(os.path.join(self.conf_path, entity))) and (entity.endswith('_conf.json')):
 
 				# Open guild configuration file
-				self.full_conf_file = os.path.join(self.conf_path, entity)
+				full_conf_file = os.path.join(self.conf_path, entity)
 				print(__file__ + ': Loading conf...' + str(entity))
 
 				guild_name = entity.split('_')[0] + entity.split('_')[1]
 
 				# Try to get json stuff
-				f = open(self.full_conf_file)
+				f = open(full_conf_file)
 				try:
 					json_data = json.load(f)
 				except:
@@ -68,11 +68,11 @@ class Template():
 					print('JSON config does not exist. Creating...')
 					data = {}
 					data['plugins'] = []
-					with open(self.full_conf_file, 'w') as f:
+					with open(full_conf_file, 'w') as f:
 						json.dump(data, f)
 
 				# Get plugin configuration
-				with open(self.full_conf_file) as f:
+				with open(full_conf_file) as f:
 					json_data = json.load(f)
 
 				the_config = None
@@ -92,7 +92,7 @@ class Template():
 					the_config['blacklisted'] = []
 					the_config['post_channel'] = ''
 					json_data['plugins'].append(the_config)
-					with open(self.full_conf_file, 'w') as f:
+					with open(full_conf_file, 'w') as f:
 						json.dump(json_data, f, indent=4)
 
 				self.guild_confs.append(the_config)
@@ -100,140 +100,6 @@ class Template():
 		print('\n\nConfigs Loaded:')
 		for config in self.guild_confs:
 			print('\t' + config['name'] + ': ' + config['guild'])
-
-	'''
-	def getGuildConfig(self, message, configs):
-		guild_config_name = message.guild.name + str(message.guild.id)
-
-		for config in configs:
-			if config['guild'] == guild_config_name:
-				return config
-
-		return {}
-
-	def hasPerms(self, message, admin_req, configs):
-		for role in message.author.roles:
-			if (role.permissions.administrator):
-				return True
-
-		if admin_req:
-			return False
-		else:
-			user_roles = []
-			for role in message.author.roles:
-				user_roles.append(role.name)
-
-			
-			config = self.getGuildConfig(message, configs)
-			if 'standard_groups' not in config:
-				return False
-			if 'admin_groups' not in config:
-				return False
-
-			for user_role in user_roles:
-				if user_role in config['standard_groups']:
-					return True
-				if user_role in config['admin_groups']:
-					return True
-
-			return False
-
-	def saveConfig(self, targ_guild, configs, conf_path):
-		for entity in os.listdir(conf_path):
-			if (os.path.isfile(os.path.join(conf_path, entity))) and (entity == str(targ_guild) + '_conf.json'):
-
-				#print('Found target conf file to save')
-
-				full_conf_file = os.path.join(conf_path, entity)
-
-				# Get plugin configuration
-				with open(full_conf_file) as f:
-					json_data = json.load(f)
-
-				
-				new_json = {}
-				new_json['plugins'] = []
-
-				for that_config in json_data['plugins']:
-					found = False
-					for this_config in configs:
-						if (that_config['name'] == this_config['name']) and (that_config['guild'] == this_config['guild']):
-							#print('Found target config to save: ' + str(that_config['name']))
-							new_json['plugins'].append(this_config)
-							found = True
-					if not found:
-						new_json['plugins'].append(that_config)
-
-				#print('Writing to configuration file: ' + str(full_conf_file))
-				with open(full_conf_file, 'w') as f:
-					json.dump(new_json, f, indent=4)
-
-				#print(json.dumps(new_json, indent=4, sort_keys=True))
-
-	async def runConfig(self, message, arg, configs, conf_path):
-		if arg[1] == 'config':
-			if not self.hasPerms(message, True, configs):
-				return True
-			embed = discord.Embed(title=str(arg[0]),
-				color=discord.Color.blue())
-			for key in configs[0].keys():
-				if isinstance(configs[0][key], str):
-					embed.add_field(name=str(key), value='set/get', inline=False)
-				else:
-					embed.add_field(name=str(key), value='add/remove/get', inline=False)
-			
-			await message.channel.send(embed=embed)
-			return True
-
-		elif arg[1] == 'get':
-			if not self.hasPerms(message, True, configs):
-				return True
-			embed = discord.Embed(title=str(arg[0]),
-				color=discord.Color.blue())
-
-			the_conf = None
-			for conf in configs:
-				if conf['guild'] == message.guild.name + str(message.guild.id):
-					the_conf = conf
-					break
-
-			if the_conf != None:
-				if str(arg[2]) in the_conf:
-					embed.add_field(name=str(arg[2]), value=str(the_conf[str(arg[2])]), inline=False)
-				else:
-					embed.add_field(name=str(arg[2]), value='Not Found', inline=False)
-
-			await message.channel.send(embed=embed)
-			return True
-
-		elif arg[1] == 'set':
-			if not self.hasPerms(message, True, configs):
-				return True
-			embed = discord.Embed(title=str(arg[0]),
-				color=discord.Color.blue())
-
-			the_conf = None
-			for conf in configs:
-				if conf['guild'] == message.guild.name + str(message.guild.id):
-					the_conf = conf
-					break
-
-			if the_conf != None:
-				if str(arg[2]) in the_conf:
-					the_conf[str(arg[2])] = arg[3]
-
-			for conf in configs:
-				if conf['guild'] == message.guild.name + str(message.guild.id):
-					conf = the_conf
-					#print(json.dumps(conf, indent=4, sort_keys=True))
-
-			self.saveConfig(message.guild.name + '_' + str(message.guild.id), configs, conf_path)
-
-			await message.channel.send(embed=embed)
-			return True
-
-		return False
-	'''
 
 	def getArgs(self, message):
 		cmd = str(message.content)
