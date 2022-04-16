71f39dbf2e5effb698e4f32fd15c60578c167221
Just Call Me Koko <at.the.red.team@gmail.com>
2022-04-16 12:45:13 -0400

    Add services config

--- plugins/plugin_services.py
+++ plugins/plugin_services.py
@@ -1,16 +1,45 @@
 import os
+import sys
 import json
 import discord
 from dotenv import load_dotenv
 from discord.ext.tasks import loop
 from requests import get
 
+sys.path.append(os.path.abspath('utils'))
+
+from utils.config_utils import ConfigUtils
+
 class Services():
+	# Required for all plugins
+	conf_path = os.path.join(os.path.dirname(__file__), 'configs')
+
+	guild_confs = []
+
+	configutils = None
+
 	name = '!services'
 
 	desc = 'Displays a list of services and their status'
 
-	synt = '!services'
+	synt = '!services [config|get <config>|set <config> <value>|add/remove <config> <value>]'
+
+	default_config = {}
+	default_config['protected'] = {}
+	default_config['protected']['name'] = __file__
+	default_config['protected']['guild'] = None
+	default_config['standard_groups'] = {}
+	default_config['standard_groups']['value'] = []
+	default_config['standard_groups']['description'] = "Authorized groups to use this command"
+	default_config['admin_groups'] = {}
+	default_config['admin_groups']['value'] = []
+	default_config['admin_groups']['description'] = "Authorized groups to use admin functions of this command"
+	default_config['blacklisted'] = {}
+	default_config['blacklisted']['value'] = []
+	default_config['blacklisted']['description'] = "Groups explicitly denied access to this command"
+	default_config['post_channel'] = {}
+	default_config['post_channel']['value'] = ""
+	default_config['post_channel']['description'] = "Desitination channel to post messages from this plugin"
 
 	looping = False
 
@@ -28,6 +57,24 @@ class Services():
 
 	def __init__(self, client = None):
 		self.client = client
+		self.configutils = ConfigUtils()
+
+		# Load configuration if it exists
+		self.guild_confs = self.configutils.loadConfig(self.conf_path, self.default_config, __file__)
+
+
+		print('\n\nConfigs Loaded:')
+		for config in self.guild_confs:
+			print('\t' + config['protected']['name'] + ': ' + config['protected']['guild'])
+
+	def getArgs(self, message):
+		cmd = str(message.content)
+		seg = str(message.content).split(' ')
+
+		if len(seg) > 1:
+			return seg
+		else:
+			return None
 
 	def checkCat(self, check_cat):
 		if self.cat == check_cat:
@@ -42,6 +89,21 @@ class Services():
 		return
 
 	async def run(self, message, obj_list):
+		# Permissions check
+		if not self.configutils.hasPerms(message, True, self.guild_confs):
+			await message.channel.send(message.author.mention + ' Permission denied')
+			return False
+
+		# Parse args
+		arg = self.getArgs(message)
+
+		# Config set/get check
+		if arg != None:
+			if await self.configutils.runConfig(message, arg, self.guild_confs, self.conf_path):
+				return True
+
+		# Do Specific Plugin Stuff
+
 		embed=discord.Embed(title="Services",
 				color=discord.Color.blue())
 		response = ''
@@ -49,10 +111,13 @@ class Services():
 		for obj in obj_list:
 			if obj.is_service:
 				response = response + str(obj.name)
-				if obj.looping:
-					response = response + ': `running`\n'
-				else:
-					response = response + ': `not running`\n'
+				try:
+					if obj.getStatus(message):
+						response = response + ': `running`\n'
+					else:
+						response = response + ': `not running`\n'
+				except:
+					response = response + ': `not running(old config)`\n'
 
 		embed.add_field(name='Status', value='```' + str(response) + '```', inline=True)
 		await message.channel.send(embed=embed)
