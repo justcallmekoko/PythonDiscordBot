ff464e2ba37cadc6dc221d414bfd999ba54ccef1
Just Call Me Koko <at.the.red.team@gmail.com>
2022-04-21 23:21:30 -0400

    Formatting

--- plugins/plugin_poll.py
+++ plugins/plugin_poll.py
@@ -26,7 +26,7 @@ class Poll():
 
 	desc = 'Start a poll and start the poll service'
 
-	synt = '!poll [<description>][; <emote> <option>]|[start]|[stop][config|get <config>|set <config> <value>|add/remove <config> <value>]\nOptions can be specified with ";" followed by an emote and the description of the option like so...\n`!poll This is a poll; <emote> Option one; <emote> Option 2; <emote> Option iii`'
+	synt = '!poll [<description>][; <emote> <option>]|[start]|[stop][config|get <config>|set <config> <value>|add/remove <config> <value>]\nOptions can be specified with ";" followed by an emote and the description of the option like so...\n!poll This is a poll; <emote> Option one; <emote> Option 2; <emote> Option iii'
 
 	default_config = {}
 	default_config['protected'] = {}
