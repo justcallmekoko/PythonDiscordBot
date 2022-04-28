b690d30c8642c7fe732a251d3f092df325e9a9a2
Just Call Me Koko <at.the.red.team@gmail.com>
2022-04-28 16:40:06 -0400

    Fix search thing

--- plugins/plugin_react_role.py
+++ plugins/plugin_react_role.py
@@ -115,7 +115,7 @@ class ReactRole():
 		target_message = None
 		for channel in message.guild.channels:
 			try:
-				target_message = await message.channel.fetch_message(id)
+				target_message = await channel.fetch_message(id)
 				print('Found ' + str(id) + ' in ' + str(channel.name))
 				break
 			except:
