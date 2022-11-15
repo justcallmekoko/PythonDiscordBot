fb3e453affbed342482ddf5ee1d8ada7dc6d57cf
Just Call Me Koko <25190487+justcallmekoko@users.noreply.github.com>
2022-11-14 22:57:46 -0500

    Update logger formatter

--- logger.py
+++ logger.py
@@ -5,10 +5,13 @@ logger = logging.getLogger("discord_bot")
 logger.setLevel(logging.DEBUG)
 
 # Add an ECS formatter to the Handler
-handler = RotatingFileHandler('/var/log/discord_bot/my_log.log', maxBytes=2000, backupCount=10)
+handler = RotatingFileHandler('/var/log/discord_bot/discord_bot.log', maxBytes=20000, backupCount=10)
+formatter = logging.Formatter('%(asctime)s %(levelname)s %(filename)s:%(lineno)d: %(message)s')
+handler.setFormatter(formatter)
 logger.addHandler(handler)
 
 streamhandler = logging.StreamHandler()
+streamhandler.setFormatter(formatter)
 logger.addHandler(streamhandler)
 
 logger.debug('Logger started')
