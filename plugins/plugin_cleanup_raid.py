f4a84a0d790eb72453aa702ab96cc435a0ff99c8
Just Call Me Koko <at.the.red.team@gmail.com>
2023-01-09 12:01:18 -0500

    UTC Stuff

--- plugins/plugin_cleanup_raid.py
+++ plugins/plugin_cleanup_raid.py
@@ -131,7 +131,7 @@ class CleanupRaid():
 		# Iterate through the members in the guild
 		for member in members:
 			# Convert the member's join time to the UTC timezone
-			member_join_time = usest.localize(member.joined_at)
+			member_join_time = pytz.utc.localize(member.joined_at)
 			# Check if the member joined at the specified time and does not have the specified role
 			if member_join_time.strftime("%Y%m%d;%H:%M") == join_time.strftime("%Y%m%d;%H:%M"): #and role not in member.roles:
 				# Add the member to the filtered list
